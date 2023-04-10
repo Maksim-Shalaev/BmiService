349bb8e272f8eb73578c934278f73d9672f64d44
Maksim Shalaev <maksimshalaev11@gmail.com>
2023-04-11 00:05:55 +0300

    Updating variable names

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,7 +1,7 @@
 public class BmiService {
 
-    public double calculate(double m, double h) {
-        double i = m / (h * h);
-        return i;
+    public double calculate(double massa, double high) {
+        double ind = massa / (high * high);
+        return ind;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,10 @@ public class Main {
 
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        double m; // масса тела в кг
-        double h; // рост тела в метрах
-        double i = service.calculate(85, 1.70);
-        int index = (int) i;
+        double massa; // масса тела в кг
+        double high; // рост тела в метрах
+        double ind = service.calculate(85, 1.70);
+        int index = (int) ind;
         System.out.println("Индекс массы тела составляет: " + index);
     }
 }
